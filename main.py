6eb528795d67770ee77b99113ab23df60e7f2e48
arex.huang <arex@null-lab.com>
2025-04-17 11:20:43 +0800

    update

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import traceback
 import colorama
 from log import log
 
+version = "1.0.1"
+
 
 def install():
 
-    workspace = os.path.abspath(os.path.dirname(__file__))
-    workspace = os.path.abspath(os.path.dirname(sys.executable))
+    if getattr(sys, "frozen", False):
+        workspace = os.path.dirname(sys.executable)
+    else:
+        workspace = os.path.dirname(os.path.abspath(__file__))
 
     log(f"workspace: {workspace}")
 
@@ -17,14 +21,20 @@ def install():
 
     log(f"arduino cli path: {arduino_cli_path}")
 
+    log("core esp32:esp32 uninstalling")
+    subprocess.run(
+        [arduino_cli_path, "core", "uninstall", "esp32:esp32"], check=False, shell=True
+    )
+
     package_index_json_path = os.path.join(
         os.getenv("LOCALAPPDATA"), "Arduino15", "package_index.json"
     )
 
     if os.path.exists(package_index_json_path):
+        log("removing package_index.json")
         os.remove(package_index_json_path)
 
-    log("core list:")
+    log("core list")
     subprocess.run([arduino_cli_path, "core", "list"], check=True, shell=True)
 
     log("modifying package_index.json")
@@ -42,11 +52,15 @@ def install():
     ) as f:
         f.write(replaced_data)
 
-    log("installing")
+    log("core esp32:esp32 installing")
     subprocess.run(
         [arduino_cli_path, "core", "install", "esp32:esp32"], check=True, shell=True
     )
-    log("install completed")
+
+    log("core list")
+    subprocess.run([arduino_cli_path, "core", "list"], check=True, shell=True)
+
+    log("core esp32:esp32 install completed")
 
 
 logo = """
@@ -58,16 +72,18 @@ logo = """
  ╚═════╝   ╚═╝   ╚═╝     ╚═╝  ╚═══╝╚══════╝   ╚═╝ 
 """
 
-title = """
+title = f"""
 Arduino ESP32 Core Installer
+version: {version}
 """
 
 try:
 
     colorama.init(autoreset=True)
-    print(colorama.Fore.CYAN + logo + title)
-    log(f'platform: {sys.platform}')
-    if not sys.platform.startswith('win32'):
+    print(colorama.Fore.CYAN + logo)
+    print(colorama.Fore.YELLOW + title)
+    log(f"platform: {sys.platform}")
+    if not sys.platform.startswith("win32"):
         raise Exception(f"unsupport platform: {sys.platform}")
     install()
     log(colorama.Fore.GREEN + "Installation succeeded")
