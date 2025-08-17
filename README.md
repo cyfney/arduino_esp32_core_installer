8a8e8b057eecefbbfdbc884dc26c31b4825fa59a
arex.huang <cyfney-anka@qq.com>
2025-08-17 13:26:44 +0800

    用户可以选择想要安装的版本

--- README.md
+++ README.md
@@ -25,12 +25,12 @@
 
 ### 解压
 
-下载完成后，找到文件arduino_esp32_core_installer.zip，使用解压软件对其进行解压操作。解压后的文件夹名称为 arduino_esp32_core_installer，这一步骤至关重要，后续的安装流程将基于此文件夹展开。
+下载完成后，找到文件arduino_esp32_core_installer_vX.Y.Z.zip，使用解压软件对其进行解压操作。解压后的文件夹名称为 arduino_esp32_core_installer_vX.Y.Z，这一步骤至关重要，后续的安装流程将基于此文件夹展开。
 
 ### 运行
 
-解压完成后，双击打开 arduino_esp32_core_installer 文件夹，在文件夹中找到名为 arduino_esp32_core_installer.exe 的可执行程序。
+- 关闭所有Arduino IDE进程
 
-双击该程序，安装进程随即启动。此时，请您耐心等待，安装程序会自动完成一系列复杂的安装操作，期间无需您进行额外的手动干预。
+- 进入到目录arduino_esp32_core_installer_vX.Y.Z，双击arduino_esp32_core_installer.vX.Y.Z.exe，安装进程随即启动。根据提示进行操作，然后耐心等待，安装程序会自动完成一系列复杂的安装操作，期间无需您进行额外的手动干预。
 
-安装接近尾声时，窗口中最后会出现```Installation succeeded```字样，这表明安装已成功达成。此时，您只需关闭该窗口，并重启 Arduino IDE，便可尽情享受 ESP32 开发板平台带来的便捷开发体验。
+- 安装成功会打印"安装成功"，然后关闭程序，正常使用Arduino IDE即可。
--- main.py
+++ main.py
@@ -7,45 +7,13 @@ from log import log
 from pathlib import Path
 import win32api
 import win32event
+import json
 
+_ARDUINO_CLI_PATH = Path() / "bin" / "arduino-cli.exe"
 
-def install():
-    log(f"workspace: {Path().absolute()}")
+_VERSION = "2.0.0"
 
-    arduino_cli_path = Path() / "bin" / "arduino-cli.exe"
-
-    log(f"arduino cli path: {arduino_cli_path}")
-
-    log("core esp32:esp32 uninstalling")
-    subprocess.run([arduino_cli_path, "core", "uninstall", "esp32:esp32"], check=False, shell=True)
-
-    package_index_json_path = Path(os.getenv("LOCALAPPDATA")) / "Arduino15" / "package_index.json"
-    log("removing package_index.json")
-    package_index_json_path.unlink(missing_ok=True)
-
-    subprocess.run([arduino_cli_path, "core", "search", "esp32:esp32"], check=True, shell=True)
-
-    log("core list")
-    subprocess.run([arduino_cli_path, "core", "list"], check=True, shell=True)
-
-    log("modifying package_index.json")
-    with package_index_json_path.open("r+", encoding="utf-8") as f:
-        content = f.read().replace("https://github.com/", "https://gh-proxy.com/https://github.com/")
-        f.seek(0)
-        f.write(content)
-
-    log("core esp32:esp32 installing")
-    subprocess.run([arduino_cli_path, "core", "install", "esp32:esp32"], check=True, shell=True)
-
-    log("core list")
-    subprocess.run([arduino_cli_path, "core", "list"], check=True, shell=True)
-
-    log("core esp32:esp32 install completed")
-
-
-VERSION = "1.1.0"
-
-LOGO = """
+_LOGO = """
  ██████╗██╗   ██╗███████╗███╗   ██╗███████╗██╗   ██╗
 ██╔════╝╚██╗ ██╔╝██╔════╝████╗  ██║██╔════╝╚██╗ ██╔╝
 ██║      ╚████╔╝ █████╗  ██╔██╗ ██║█████╗   ╚████╔╝ 
@@ -54,27 +22,97 @@ LOGO = """
  ╚═════╝   ╚═╝   ╚═╝     ╚═╝  ╚═══╝╚══════╝   ╚═╝ 
 """
 
-TITLE = f"""
+_TITLE = f"""
 Arduino ESP32 Core Installer
-version: {VERSION}
+version: {_VERSION}
 """
 
-try:
 
+def get_expected_version():
+    log(colorama.Fore.YELLOW + colorama.Style.BRIGHT + "开始查询esp32:esp32版本号信息，请稍等")
+    ret = subprocess.run(
+        [_ARDUINO_CLI_PATH, "core", "search", "esp32:esp32", "-a", "--json"], check=True, shell=True, encoding="utf-8", capture_output=True
+    )
+    json_obj = json.loads(ret.stdout)
+    log(colorama.Fore.YELLOW + "\033[1;33m所有esp32:esp32版本号:\033[0m")
+    versions = set()
+    for index, version in enumerate(json_obj["platforms"][0]["releases"].items(), start=1):
+        versions.add(version[0])
+        print(colorama.Fore.MAGENTA + colorama.Style.BRIGHT + f"{version[0]}".ljust(12), end="")
+        if index % 4 == 0:
+            print("")
+        else:
+            print("", end=" ")
+
+    while True:
+        version = input("\033[1;33m" "请输入需要安装的版本号（格式 X.Y.Z，例如：3.2.0，留空代表安装最新版）→ 按回车确认:\n" "\033[0m")
+        version = version.strip()
+
+        if version == "":
+            return None
+        elif version in versions:
+            return version
+        else:
+            log(colorama.Fore.RED + colorama.Style.BRIGHT + f"输入的版本号 {version} 不存在，请重新输入：")
+
+
+def install():
+    log(f"workspace: {Path().absolute()}")
+
+    log(f"arduino cli path: {_ARDUINO_CLI_PATH}")
+
+    version = get_expected_version()
+    if version is None:
+        log(colorama.Fore.YELLOW + colorama.Style.BRIGHT + f"开始安装最新版esp32:esp32，请稍等")
+    else:
+        log(colorama.Fore.YELLOW + colorama.Style.BRIGHT + f"开始安装 esp32:esp32@{version}，请稍等")
+
+    log("core esp32:esp32 uninstalling")
+    subprocess.run([_ARDUINO_CLI_PATH, "core", "uninstall", "esp32:esp32"], check=False, shell=True)
+
+    package_index_json_path = Path(os.getenv("LOCALAPPDATA")) / "Arduino15" / "package_index.json"
+    log("removing package_index.json")
+    package_index_json_path.unlink(missing_ok=True)
+
+    # subprocess.run([_ARDUINO_CLI_PATH, "core", "search", "esp32:esp32"], check=True, shell=True)
+
+    log("core list")
+    subprocess.run([_ARDUINO_CLI_PATH, "core", "list"], check=True, shell=True)
+
+    log("modifying package_index.json")
+    with package_index_json_path.open("r+", encoding="utf-8") as f:
+        content = f.read().replace("https://github.com/", "https://gh-proxy.com/https://github.com/")
+        f.seek(0)
+        f.write(content)
+
+    if version is None:
+        log("core esp32:esp32 installing")
+        subprocess.run([_ARDUINO_CLI_PATH, "core", "install", "esp32:esp32"], check=True, shell=True)
+    else:
+        log(f"core esp32:esp32 {version} installing")
+        subprocess.run([_ARDUINO_CLI_PATH, "core", "install", f"esp32:esp32@{version}"], check=True, shell=True)
+
+    log("core list")
+    subprocess.run([_ARDUINO_CLI_PATH, "core", "list"], check=True, shell=True)
+
+    log("core esp32:esp32 install completed")
+
+
+try:
     colorama.init(autoreset=True)
-    print(colorama.Fore.CYAN + LOGO)
-    print(colorama.Fore.YELLOW + TITLE)
+    print(colorama.Fore.CYAN + colorama.Style.BRIGHT + _LOGO)
+    print(colorama.Fore.YELLOW + colorama.Style.BRIGHT + _TITLE)
     log(f"platform: {sys.platform}")
     if not sys.platform.startswith("win32"):
-        raise Exception(f"unsupport platform: {sys.platform}")
+        raise Exception(f"不支持当前操作系统: {sys.platform}")
     mutex = win32event.CreateMutex(None, False, "cyfney_arduino_esp32_core_installer")
     if win32api.GetLastError() == 183:
         raise RuntimeError("检测到程序已在运行，请关闭其他实例后重试")
     install()
-    log(colorama.Fore.GREEN + "Installation succeeded")
+    log(colorama.Fore.GREEN + colorama.Style.BRIGHT + "安装成功")
 except Exception:
     log(colorama.Fore.RED + traceback.format_exc())
-    log(colorama.Fore.RED + "Installation failed")
+    log(colorama.Fore.RED + colorama.Style.BRIGHT + "安装失败")
 
-log(colorama.Fore.YELLOW + "Press any key to close the window")
+log(colorama.Fore.YELLOW + colorama.Style.BRIGHT + "请输入任意按键关闭窗口")
 input()
